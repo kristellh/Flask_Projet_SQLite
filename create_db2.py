5a2d9505873c844174d1ff56ec3e0dda3ee9c74d
kristellh <k.hervo@gmail.com>
2025-03-19 11:03:09 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -10,6 +10,8 @@ cur = connection.cursor()
 
 cur.execute("INSERT INTO livres (id, created, titre, auteur, date_pret,utilisateur_pret) VALUES (?,?,?, ?, ?,?)",(1,190325,'A la recherche du temps perdu', 'Marcel Proust',' ',''))
 cur.execute("INSERT INTO livres (id, created,titre, auteur, date_pret,utilisateur_pret) VALUES (?,?,?, ?, ?,?)",(2,190325,'Notre dame de Paris', 'Victor Hugo',' ',''))
+cur.execute("INSERT INTO stock (id, created, titre, auteur,quantite_en_stock) VALUES (?,?,?, ?, ?)",(1,190325,'A la recherche du temps perdu', 'Marcel Proust',2))
+cur.execute("INSERT INTO stock (id, created, titre, auteur,quantite_en_stock) VALUES (?,?,?, ?, ?,?)",(2,190325,'Notre dame de Paris', 'Victor Hugo',3))
 
 connection.commit()
 connection.close()
