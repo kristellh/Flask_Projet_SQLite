1988615c6b9a2d4a55990989bb02cf4750ed59ed
kristellh <k.hervo@gmail.com>
2025-03-19 11:21:18 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 connection = sqlite3.connect('database2.db')
@@ -8,11 +7,23 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO livres (id, created, titre, auteur,pret) VALUES (?,?,?, ?)",(1,190325,'A la recherche du temps perdu', 'Marcel Proust','disponible'))
-cur.execute("INSERT INTO livres (id, created,titre, auteur, date_pret,utilisateur_pret) VALUES (?,?,?, ?)",(2,190325,'Notre dame de Paris', 'Victor Hugo','oui'))
-cur.execute("INSERT INTO stock (id, created, titre, auteur,quantite_en_stock) VALUES (?,?,?, ?, ?)",(1,190325,'A la recherche du temps perdu', 'Marcel Proust',2))
-cur.execute("INSERT INTO stock (id, created, titre, auteur,quantite_en_stock) VALUES (?,?,?, ?, ?)",(2,190325,'Notre dame de Paris', 'Victor Hugo',3))
-cur.execute("INSERT INTO pret (id, created, titre, auteur,date_pret,utilisateur_pret) VALUES (?,?,?, ?, ?,?)",(1,190325,'A la recherche du temps perdu','Marcel Proust',12/2/2025,'Dupond'))
-cur.execute("INSERT INTO pret (id, created, titre, auteur,date_pret,utilisateur_pret) VALUES (?,?,?, ?, ?,?)",(2,190325,'Notre dame de Paris', 'Victor Hugo',12/2/2025,'Martin'))
+# Insérer dans la table livres
+cur.execute("INSERT INTO livres (id, created, titre, auteur, pret) VALUES (?, ?, ?, ?, ?)",
+            (1, 190325, 'A la recherche du temps perdu', 'Marcel Proust', 'disponible'))
+cur.execute("INSERT INTO livres (id, created, titre, auteur, pret) VALUES (?, ?, ?, ?, ?)",
+            (2, 190325, 'Notre dame de Paris', 'Victor Hugo', 'disponible'))
+
+# Insérer dans la table stock
+cur.execute("INSERT INTO stock (id, created, titre, auteur, quantite_en_stock) VALUES (?, ?, ?, ?, ?)",
+            (1, 190325, 'A la recherche du temps perdu', 'Marcel Proust', 2))
+cur.execute("INSERT INTO stock (id, created, titre, auteur, quantite_en_stock) VALUES (?, ?, ?, ?, ?)",
+            (2, 190325, 'Notre dame de Paris', 'Victor Hugo', 3))
+
+# Insérer dans la table pret avec la date correctement formatée
+cur.execute("INSERT INTO pret (id, created, titre, auteur, date_pret, utilisateur_pret) VALUES (?, ?, ?, ?, ?, ?)",
+            (1, 190325, 'A la recherche du temps perdu', 'Marcel Proust', '2025-02-12', 'Dupond'))
+cur.execute("INSERT INTO pret (id, created, titre, auteur, date_pret, utilisateur_pret) VALUES (?, ?, ?, ?, ?, ?)",
+            (2, 190325, 'Notre dame de Paris', 'Victor Hugo', '2025-02-12', 'Martin'))
+
 connection.commit()
 connection.close()
