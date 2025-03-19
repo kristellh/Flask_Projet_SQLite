d25d66177e3a61ccae655a53ef24c917e953af7d
kristellh <k.hervo@gmail.com>
2025-03-19 10:21:03 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -1,1 +1,15 @@
 
+import sqlite3
+
+connection = sqlite3.connect('database2.db')
+
+with open('schema2.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO livres (titre, auteur, date_pret,utilisateur_pret) VALUES (?, ?, ?,?,?)",('A la recherche du temps perdu', 'Marcel Proust', ' ', ''))
+cur.execute("INSERT INTO livres (titre, auteur, date_pret,utilisateur_pret) VALUES (?, ?, ?,?,?)",('Notre dame de Paris', 'Victor Hugo', ' ', ''))
+
+connection.commit()
+connection.close()
