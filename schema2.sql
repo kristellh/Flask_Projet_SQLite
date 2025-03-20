c07ae7ead4398c35c1f73652800ebae6dc48b022
kristellh <k.hervo@gmail.com>
2025-03-20 11:43:16 +0100

    Update schema2.sql

--- schema2.sql
+++ schema2.sql
@@ -4,7 +4,8 @@ CREATE TABLE livres (
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     titre TEXT NOT NULL,
     auteur TEXT NOT NULL,
-    pret TEXT NOT NULL
+    pret TEXT NOT NULL,
+    quantite_en_stock INTEGER
 );
 DROP TABLE IF EXISTS stock;
 CREATE TABLE stock (
